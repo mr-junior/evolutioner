d0ac6114674f4d7446b2f16ba56126bfe1953d07
mr-junior <hovhanminas@mail.ru>
2014-11-15 15:54:35 +0400

    Correcting spacings and style

--- random_switch_task.cpp
+++ random_switch_task.cpp
@@ -4,10 +4,10 @@
 namespace graph_randomization
 {
 
-random_switch_task::random_switch_task(const undirected_graph& graph, double mu, size_t step_count, size_t graph_step)
-  : base_task(graph, mu, step_count, graph_step)
+random_switch_task::random_switch_task(const undirected_graph& graph, double mu, size_t step_count, size_t graph_step) :
+  base_task(graph, mu, step_count, graph_step)
 {
-	std::cout << __PRETTY_FUNCTION__ << std::endl;
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
   size_t vertices_count = boost::num_vertices(graph_);
   size_t all_edges_count = vertices_count * (vertices_count - 1) / 2;
   size_t non_existing_edges_count = all_edges_count - boost::num_edges(graph_);
@@ -17,10 +17,10 @@ random_switch_task::random_switch_task(const undirected_graph& graph, double mu,
   size_t index = 0;
   for(; v != v_end; ++v)
   {
-		vertex_iterator v1 = v;
+    vertex_iterator v1 = v;
     for(++v1; v1 != v_end; ++v1)
     {
-      if(false == boost::edge(*v, *v1, graph_).second)
+      if(!boost::edge(*v, *v1, graph_).second)
       {
         non_existing_edges_.push_back(std::make_pair(*v, *v1));
       }
@@ -54,7 +54,7 @@ void random_switch_task::make_randomization_step()
       adjacency_iterator v, v_end;
       for(boost::tie(v, v_end) = boost::adjacent_vertices(source1, graph_); v != v_end; ++v)
       {
-        if(*v != target1 && true == boost::edge(*v, target1, graph_).second)
+        if(*v != target1 && boost::edge(*v, target1, graph_).second)
         {
           ++removed;
         }
@@ -70,28 +70,28 @@ void random_switch_task::make_randomization_step()
     adjacency_iterator v, v_end;
     for(boost::tie(v, v_end) = boost::adjacent_vertices(source1, graph_); v != v_end; ++v)
     {
-      if(*v != target1 && true == boost::edge(*v, target1, graph_).second)
+      if(*v != target1 && boost::edge(*v, target1, graph_).second)
       {
         ++removed;
       }
     }
   }
 
-	boost::graph_traits<undirected_graph>::edges_size_type added_edge_num = 0;
-	std::pair<vertex, vertex> edge_to_add;
-	assert(non_existing_edges_.size() > 0);
-	added_edge_num = (*var_generator_)();
-	edge_to_add = non_existing_edges_[added_edge_num];
-	assert(false == boost::edge(edge_to_add.first, edge_to_add.second, graph_).second);
+  boost::graph_traits<undirected_graph>::edges_size_type added_edge_num = 0;
+  std::pair<vertex, vertex> edge_to_add;
+  assert(non_existing_edges_.size() > 0);
+  added_edge_num = (*var_generator_)();
+  edge_to_add = non_existing_edges_[added_edge_num];
+  assert(!boost::edge(edge_to_add.first, edge_to_add.second, graph_).second);
 
-	adjacency_iterator v, v_end;
-	for(boost::tie(v, v_end) = boost::adjacent_vertices(edge_to_add.first, graph_); v != v_end; ++v)
-	{
-		if(true == boost::edge(*v, edge_to_add.second, graph_).second)
-		{
-			++added;
-		}
-	}
+  adjacency_iterator v, v_end;
+  for(boost::tie(v, v_end) = boost::adjacent_vertices(edge_to_add.first, graph_); v != v_end; ++v)
+  {
+    if(boost::edge(*v, edge_to_add.second, graph_).second)
+    {
+      ++added;
+    }
+  }
 
   if(0 != thread1)
   {
@@ -99,16 +99,16 @@ void random_switch_task::make_randomization_step()
     delete thread1;
   }
 
-  if(edge_to_add.first == source1 && true == boost::edge(edge_to_add.second, target1, graph_).second ||
-     edge_to_add.first == target1 && true == boost::edge(edge_to_add.second, source1, graph_).second ||
-     edge_to_add.second == source1 && true == boost::edge(edge_to_add.first, target1, graph_).second ||
-     edge_to_add.second == target1 && true == boost::edge(edge_to_add.first, source1, graph_).second)
+  if(edge_to_add.first == source1 && boost::edge(edge_to_add.second, target1, graph_).second ||
+     edge_to_add.first == target1 && boost::edge(edge_to_add.second, source1, graph_).second ||
+     edge_to_add.second == source1 && boost::edge(edge_to_add.first, target1, graph_).second ||
+     edge_to_add.second == target1 && boost::edge(edge_to_add.first, source1, graph_).second)
   {
     --added;
   }
 
   int delta = added - removed;
-  if(false == check_step(delta))
+  if(!check_step(delta))
   {
     return;
   }
