eeaade3e0a3eb7ee49d51152f957993f60843604
mr-junior <hovhanminas@mail.ru>
2014-11-15 15:36:51 +0400

    Correcting spacings and style

--- fixed_degree_task.h
+++ fixed_degree_task.h
@@ -1,5 +1,4 @@
-#ifndef _FIXED_DEGREE_TASK_H_
-#define _FIXED_DEGREE_TASK_H_
+#pragma once
 
 #include "base_task.h"
 
@@ -10,10 +9,8 @@ class fixed_degree_task : public base_task
 {
 public:
   fixed_degree_task(const undirected_graph& graph, double mu, int step_count, size_t graph_step);
-	~fixed_degree_task();
-	virtual void make_randomization_step();
+  ~fixed_degree_task();
+  virtual void make_randomization_step();
 };
 
-} // end namespace graph_randomization
-
-#endif // _FIXED_DEGREE_TASK_H_
+}
