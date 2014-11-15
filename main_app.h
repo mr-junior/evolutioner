b9c5288129400b0b3abc33601db23a821bcdc12c
mr-junior <hovhanminas@mail.ru>
2014-11-15 15:52:17 +0400

    Correcting spacings and style

--- main_app.h
+++ main_app.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <boost/mpi.hpp>
 #include <string>
 #include <defs.h>
@@ -21,23 +23,23 @@ class main_app
     void execute_main_process();
     void execute_secondary_process();
 
-		void distribute_data();
-		void receive_data();
+    void distribute_data();
+    void receive_data();
     void collect_results();
     void finalize();
     void read_config_file(); // for future
-		void load_graph_data();
-		void load_mu_data();
+    void load_graph_data();
+    void load_mu_data();
     void prepare_output_directory();
     void write_output(double mu, const std::vector<std::pair<size_t, double>>& result) const;
-		void write_output(double mu, const std::vector<graph_randomization::undirected_graph>& result) const;
+    void write_output(double mu, const std::vector<graph_randomization::undirected_graph>& result) const;
 
-		struct graph_data
-		{
-			graph_randomization::undirected_graph graph_;
-			size_t vertex_count_;
-			double probability_;
-		};
+    struct graph_data
+    {
+      graph_randomization::undirected_graph graph_;
+      size_t vertex_count_;
+      double probability_;
+    };
 
   private:
     int argc_;
@@ -47,13 +49,13 @@ class main_app
     mpi::communicator world_;
     size_t rank_;
     size_t size_;
-		std::map<size_t, size_t> process_rank_to_mu_count_;
+    std::map<size_t, size_t> process_rank_to_mu_count_;
     std::string output_directory_;
     std::string graph_file_name_;
     std::string mu_file_name_;
-		graph_data gr_data_;
-		std::vector<double> mu_values_;
+    graph_data gr_data_;
+    std::vector<double> mu_values_;
     size_t step_count_;
     size_t pass_count_;
-		size_t graph_step_;
+    size_t graph_step_;
 };
