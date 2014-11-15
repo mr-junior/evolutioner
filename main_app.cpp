45dccc7386e39da23428c8fa9ad01a1cd7a8719e
mr-junior <hovhanminas@mail.ru>
2014-11-15 15:50:37 +0400

    Correcting spacings and style

--- main_app.cpp
+++ main_app.cpp
@@ -26,21 +26,21 @@ enum class message_tag : int
   vertex_count = 1,
   probability,
   graph,
-	step_count,
+  step_count,
   pass_count,
-	graph_step,
-	mu_values_vector,
-	sync,
-	task_type,
+  graph_step,
+  mu_values_vector,
+  sync,
+  task_type,
   results_base = 1000,
-	results_graphs = 10000
+  results_graphs = 10000
 };
 
 //validator for type field
 struct randomization_type
 {
-  randomization_type(const std::string& value)
-    : value_(value)
+  randomization_type(const std::string& value) :
+    value_(value)
   {}
   std::string value_;
 };
@@ -92,7 +92,7 @@ bool main_app::parse_command_line()
     ("step_count,c", po::value<size_t>(), "Step count per generation.")
     ("pass_count,p", po::value<size_t>(), "Pass count for each mu value.")
     ("type,t", po::value<randomization_type>(), "Randomization type.")
-		("graph_step,s", po::value<size_t>(), "Step count to dump graph from.");
+    ("graph_step,s", po::value<size_t>(), "Step count to dump graph from.");
 
   po::variables_map vm;
   try
@@ -101,7 +101,7 @@ bool main_app::parse_command_line()
     if(vm.count("help"))
     {
       std::cout << "Graph Randomizer v1.0" << std::endl << desc << std::endl;
-			return false;
+      return false;
     }
     if(vm.count("graph_file"))
     {
@@ -128,31 +128,31 @@ bool main_app::parse_command_line()
       std::cout << "type = " << vm["type"].as<randomization_type>().value_ << std::endl;
       type_ = vm["type"].as<randomization_type>().value_;
     }
-		if(vm.count("graph_step"))
-		{
+    if(vm.count("graph_step"))
+    {
       std::cout << "graph_step = " << vm["graph_step"].as<size_t>() << std::endl;
       graph_step_ = vm["graph_step"].as<size_t>();
-		}
+    }
     po::notify(vm);
   }
   catch(po::error& e)
   {
     std::cerr << "\nError parsing command line: " << e.what() << std::endl << std::endl;
     std::cerr << desc << std::endl;
-		return false;
+    return false;
   }
-	return true;
+  return true;
 }
 
 void main_app::execute()
 {
   assert(1 <= size_);
-	if(0 == rank_ && false == parse_command_line())
-	{
-		finalize();
-		env_.abort(-1);
-	}
-	world_.barrier();
+  if(0 == rank_ && false == parse_command_line())
+  {
+    finalize();
+    env_.abort(-1);
+  }
+  world_.barrier();
   if(1 == size_)
   {
     execute_with_single_process();
@@ -165,14 +165,14 @@ void main_app::execute()
 
 void main_app::execute_with_single_process()
 {
-	load_graph_data();
-	load_mu_data();
+  load_graph_data();
+  load_mu_data();
   std::cerr << "NOT IMPLEMENTED YET." << std::endl;
 }
 
 void main_app::execute_with_multiple_processes()
 {
-	std::cout << rank_ << " " << __PRETTY_FUNCTION__ << std::endl;
+  std::cout << rank_ << " " << __PRETTY_FUNCTION__ << std::endl;
   if(0 == rank_)
   {
     execute_main_process();
@@ -191,7 +191,7 @@ void main_app::finalize()
 
 void main_app::load_graph_data()
 {
-	assert(false == graph_file_name_.empty());
+  assert(false == graph_file_name_.empty());
   std::ifstream graph_file;
   graph_file.open(graph_file_name_);
   if(false == graph_file.is_open())
@@ -202,62 +202,62 @@ void main_app::load_graph_data()
   }
   archive::text_iarchive ia(graph_file);
   ia >> gr_data_.vertex_count_ >> gr_data_.probability_;
-	//std::vector< boost::property<boost::edge_index_t, std::size_t> > edge_properties;
-	//edge_properties.resize(gr_data_.vertex_count_);
-	//for(size_t i = 0; i < gr_data_.vertex_count_; ++i)
-	//{
-	//	edge_properties[i].m_value = i;
-	//}
-	//using namespace graph_randomization;
-	//ugraph_without_edge_indexes graph;
+  //std::vector< boost::property<boost::edge_index_t, std::size_t> > edge_properties;
+  //edge_properties.resize(gr_data_.vertex_count_);
+  //for(size_t i = 0; i < gr_data_.vertex_count_; ++i)
+  //{
+  //	edge_properties[i].m_value = i;
+  //}
+  //using namespace graph_randomization;
+  //ugraph_without_edge_indexes graph;
   //serialization::load(ia, graph, 0);
-	//std::map<vertex, size_t> vertex_to_index;
-	//vertex_iterator vertex_it, vertex_it_end;
-	//boost::tie(vertex_it, vertex_it_end) = boost::vertices(graph);
-	//for(size_t index = 0; vertex_it != vertex_it_end; ++vertex_it, ++index)
-	//{
-	//	vertex_to_index[*vertex_it] = index;
-	//}
-	//std::vector< std::pair<size_t, size_t> > edges;
-	//boost::graph_traits<ugraph_without_edge_indexes>::edge_iterator edge_it, edge_it_end;
-	//boost::tie(edge_it, edge_it_end) = boost::edges(graph);
-	//for(; edge_it != edge_it_end; ++edge_it)
-	//{
-	//	edge e = *edge_it;
-	//	vertex source = boost::source(e, graph);
-	//	vertex target = boost::target(e, graph);
-	//	edges.push_back(std::make_pair(vertex_to_index[source], vertex_to_index[target]));
-	//}
-	//gr_data_.graph_ = graph_randomization::undirected_graph(edges.begin(), 
-	//																												edges.end(), 
-	//																												edge_properties.begin(), 
-	//																												gr_data_.vertex_count_);
+  //std::map<vertex, size_t> vertex_to_index;
+  //vertex_iterator vertex_it, vertex_it_end;
+  //boost::tie(vertex_it, vertex_it_end) = boost::vertices(graph);
+  //for(size_t index = 0; vertex_it != vertex_it_end; ++vertex_it, ++index)
+  //{
+  //	vertex_to_index[*vertex_it] = index;
+  //}
+  //std::vector< std::pair<size_t, size_t> > edges;
+  //boost::graph_traits<ugraph_without_edge_indexes>::edge_iterator edge_it, edge_it_end;
+  //boost::tie(edge_it, edge_it_end) = boost::edges(graph);
+  //for(; edge_it != edge_it_end; ++edge_it)
+  //{
+  //	edge e = *edge_it;
+  //	vertex source = boost::source(e, graph);
+  //	vertex target = boost::target(e, graph);
+  //	edges.push_back(std::make_pair(vertex_to_index[source], vertex_to_index[target]));
+  //}
+  //gr_data_.graph_ = graph_randomization::undirected_graph(edges.begin(), 
+  //																												edges.end(), 
+  //																												edge_properties.begin(), 
+  //																												gr_data_.vertex_count_);
   serialization::load(ia, gr_data_.graph_, 0);
-	graph_file.close();
+  graph_file.close();
 }
 
 void main_app::load_mu_data()
 {
-	assert(false == mu_file_name_.empty());
+  assert(!mu_file_name_.empty());
   std::ifstream mu_file;
   mu_file.open(mu_file_name_);
-  if(false == mu_file.is_open())
+  if(!mu_file.is_open())
   {
     std::cerr << "Failed to open file containing mu values." << std::endl;
     finalize();
     env_.abort(-1);
   }
-	double mu = 0.0;
-	while(mu_file >> mu)
-	{
-		mu_values_.push_back(mu);
-	}
-	mu_file.close();
+  double mu = 0.0;
+  while(mu_file >> mu)
+  {
+    mu_values_.push_back(mu);
+  }
+  mu_file.close();
 }
 
 void main_app::distribute_data()
 {
-	size_t mu_count = mu_values_.size();
+  size_t mu_count = mu_values_.size();
   size_t mu_per_process = mu_count / (size_ - 1);
   size_t remainder = 0;
   if(0 == mu_per_process)
@@ -273,7 +273,7 @@ void main_app::distribute_data()
   {
     world_.send(i, static_cast<int>(message_tag::vertex_count), gr_data_.vertex_count_);
     world_.send(i, static_cast<int>(message_tag::probability), gr_data_.probability_);
-		world_.send(i, static_cast<int>(message_tag::graph), gr_data_.graph_);
+    world_.send(i, static_cast<int>(message_tag::graph), gr_data_.graph_);
     world_.send(i, static_cast<int>(message_tag::step_count), step_count_);
     world_.send(i, static_cast<int>(message_tag::pass_count), pass_count_);
     world_.send(i, static_cast<int>(message_tag::graph_step), graph_step_);
@@ -284,14 +284,13 @@ void main_app::distribute_data()
       ++mu_per_process_i;
       --remainder;
     }
-		std::vector<double> mu_values_for_process_i(mu_values_.begin() + base_index, mu_values_.begin() + base_index + mu_per_process_i);
-		world_.send(i, static_cast<int>(message_tag::mu_values_vector), mu_values_for_process_i);
-		process_rank_to_mu_count_[i] = mu_per_process_i;
+    std::vector<double> mu_values_for_process_i(mu_values_.begin() + base_index, mu_values_.begin() + base_index + mu_per_process_i);
+    world_.send(i, static_cast<int>(message_tag::mu_values_vector), mu_values_for_process_i);
+    process_rank_to_mu_count_[i] = mu_per_process_i;
     base_index += mu_per_process_i;
 
-		world_.send(i, static_cast<int>(message_tag::task_type), type_);
-
-		world_.recv(i, static_cast<int>(message_tag::sync));
+    world_.send(i, static_cast<int>(message_tag::task_type), type_);
+    world_.recv(i, static_cast<int>(message_tag::sync));
   }
 }
 
@@ -299,11 +298,11 @@ void main_app::collect_results()
 {
   std::vector<std::pair<mpi::request, int>> reqs;
   std::vector<std::pair<mpi::request, int>> graph_reqs;
-	size_t mu_count = mu_values_.size();
+  size_t mu_count = mu_values_.size();
   reqs.resize(mu_count);
   graph_reqs.resize(mu_count);
   std::vector<std::pair<size_t, double>>* results = new std::vector<std::pair<size_t, double>>[mu_count];
-	std::vector<graph_randomization::undirected_graph>* final_graphs = new std::vector<graph_randomization::undirected_graph>[mu_count];
+  std::vector<graph_randomization::undirected_graph>* final_graphs = new std::vector<graph_randomization::undirected_graph>[mu_count];
   size_t base_index = 0;
   for(size_t i = 1; i < size_; ++i)
   {
@@ -314,7 +313,7 @@ void main_app::collect_results()
     }
     base_index += process_rank_to_mu_count_[i];
   }
-	base_index = 0;
+  base_index = 0;
   for(size_t i = 1; i < size_; ++i)
   {
     for(size_t j = 0; j < process_rank_to_mu_count_[i]; ++j)
@@ -338,8 +337,8 @@ void main_app::collect_results()
     usleep(100);
   }
   delete[] results;
-	while(false == graph_reqs.empty())
-	{
+  while(false == graph_reqs.empty())
+  {
     for(int i = graph_reqs.size() - 1; i >= 0; --i)
     {
       if(true == graph_reqs[i].first.test().is_initialized())
@@ -349,19 +348,19 @@ void main_app::collect_results()
       }
     }
     usleep(100);
-	}
-	delete[] final_graphs;
+  }
+  delete[] final_graphs;
   std::cout << rank_ << " collected all information" << std::endl;
 }
 
 void main_app::execute_main_process()
 {
-	std::cout << rank_ << " " << __PRETTY_FUNCTION__ << std::endl;
+  std::cout << rank_ << " " << __PRETTY_FUNCTION__ << std::endl;
   assert(1 < size_);
-	load_graph_data();
-	load_mu_data();
-	distribute_data();
-	collect_results();
+  load_graph_data();
+  load_mu_data();
+  distribute_data();
+  collect_results();
 }
 
 void main_app::receive_data()
@@ -373,52 +372,52 @@ void main_app::receive_data()
   world_.recv(0, static_cast<int>(message_tag::pass_count), pass_count_);
   world_.recv(0, static_cast<int>(message_tag::graph_step), graph_step_);
   world_.recv(0, static_cast<int>(message_tag::mu_values_vector), mu_values_);
-	world_.recv(0, static_cast<int>(message_tag::task_type), type_);
-	world_.send(0, static_cast<int>(message_tag::sync));
+  world_.recv(0, static_cast<int>(message_tag::task_type), type_);
+  world_.send(0, static_cast<int>(message_tag::sync));
 }
 
 void main_app::execute_secondary_process()
 {
-	std::cout << rank_ << " " << __PRETTY_FUNCTION__ << std::endl;
-	receive_data();
-	size_t mu_count = mu_values_.size();
+  std::cout << rank_ << " " << __PRETTY_FUNCTION__ << std::endl;
+  receive_data();
+  size_t mu_count = mu_values_.size();
   mpi::request* reqs = new mpi::request[mu_count];
-	mpi::request* graph_reqs = new mpi::request[mu_count];
+  mpi::request* graph_reqs = new mpi::request[mu_count];
   for(size_t i = 0; i < mu_count; ++i)
   {
-		std::vector<std::pair<size_t, double>> results;
-		std::vector<graph_randomization::undirected_graph> graphs;
-		for(size_t p = 0; p < pass_count_; ++p)
+    std::vector<std::pair<size_t, double>> results;
+    std::vector<graph_randomization::undirected_graph> graphs;
+    for(size_t p = 0; p < pass_count_; ++p)
     {
-			std::shared_ptr<graph_randomization::base_task> task = graph_randomization::get_task(gr_data_.graph_, mu_values_[i], step_count_, graph_step_, type_); 
+      std::shared_ptr<graph_randomization::base_task> task = graph_randomization::get_task(gr_data_.graph_, mu_values_[i], step_count_, graph_step_, type_); 
       task->perform_randomization();
       std::vector<std::pair<size_t, size_t>> results_for_i = task->results();
-			results.resize(results_for_i.size());
-			for(size_t k = 0; k < results.size(); ++k)
-			{
-				results[k].first = k;
-			}
-			//assert(step_count_ + 1 == results_for_i.size());
-			for(size_t j = 0; j < results_for_i.size(); ++j)
-			{
-				assert(results[j].first == results_for_i[j].first);
-				results[j].second += static_cast<double>(results_for_i[j].second);
-			}
-			//graph.clear();
-			//boost::copy_graph(task->graph(), graph);
-			graphs = task->graphs();
-		}
-		for(size_t j = 0; j < results.size(); ++j)
-		{
-			results[j].second /= pass_count_;
-		}
-		reqs[i] = world_.isend(0, static_cast<int>(message_tag::results_base)*rank_ + i, results);
-		graph_reqs[i] = world_.isend(0, static_cast<int>(message_tag::results_graphs)*rank_ + i, graphs);
-	}
-	mpi::wait_all(reqs, reqs + mu_count);
-	mpi::wait_all(graph_reqs, graph_reqs + mu_count);
-	delete[] reqs;
-	delete[] graph_reqs;
+      results.resize(results_for_i.size());
+      for(size_t k = 0; k < results.size(); ++k)
+      {
+        results[k].first = k;
+      }
+      //assert(step_count_ + 1 == results_for_i.size());
+      for(size_t j = 0; j < results_for_i.size(); ++j)
+      {
+        assert(results[j].first == results_for_i[j].first);
+        results[j].second += static_cast<double>(results_for_i[j].second);
+      }
+      //graph.clear();
+      //boost::copy_graph(task->graph(), graph);
+      graphs = task->graphs();
+    }
+    for(size_t j = 0; j < results.size(); ++j)
+    {
+      results[j].second /= pass_count_;
+    }
+    reqs[i] = world_.isend(0, static_cast<int>(message_tag::results_base)*rank_ + i, results);
+    graph_reqs[i] = world_.isend(0, static_cast<int>(message_tag::results_graphs)*rank_ + i, graphs);
+  }
+  mpi::wait_all(reqs, reqs + mu_count);
+  mpi::wait_all(graph_reqs, graph_reqs + mu_count);
+  delete[] reqs;
+  delete[] graph_reqs;
 }
 
 void main_app::prepare_output_directory()
@@ -436,22 +435,22 @@ void main_app::prepare_output_directory()
 
 void main_app::write_output(double mu, const std::vector<std::pair<size_t, double>>& result) const
 {
-	//xlslib_core::workbook* wrkbk = new xlslib_core::workbook();
-	//xlslib_core::worksheet* wrksht = wrkbk->sheet("Trajectory");
+  //xlslib_core::workbook* wrkbk = new xlslib_core::workbook();
+  //xlslib_core::worksheet* wrksht = wrkbk->sheet("Trajectory");
 
   //for(size_t i = 0; i < result.size(); ++i)
   //{
-	//	wrksht->number(i, 0, static_cast<double>(i), xlslib_core::FMT_GENERAL, 0);
-	//	wrksht->number(i, 1, result[i].second, xlslib_core::FMT_GENERAL, 0);
+  //	wrksht->number(i, 0, static_cast<double>(i), xlslib_core::FMT_GENERAL, 0);
+  //	wrksht->number(i, 1, result[i].second, xlslib_core::FMT_GENERAL, 0);
   //  //output << result[i].first << " " << result[i].second << std::endl;
   //}
-	//std::cout << rank_ << " " << __PRETTY_FUNCTION__ << std::endl;
+  //std::cout << rank_ << " " << __PRETTY_FUNCTION__ << std::endl;
   std::stringstream file_name;
   file_name << (output_directory_.empty() ? "" : output_directory_ + "/") <<"N" << gr_data_.vertex_count_ 
-						<< "_p" << gr_data_.probability_ << "_u" << mu << "_T";
+    << "_p" << gr_data_.probability_ << "_u" << mu << "_T";
   //try
   //{
-    fs::create_directory(file_name.str());
+  fs::create_directory(file_name.str());
   //}
   //catch(fs::basic_filesystem_error&)
   //{
@@ -460,7 +459,7 @@ void main_app::write_output(double mu, const std::vector<std::pair<size_t, doubl
   //}
   file_name << "/N" << gr_data_.vertex_count_ << "_p" << gr_data_.probability_ << "_u" << mu << ".txt";
   //file_name << "/" << 0 << ".xls";
-	//wrkbk->Dump(file_name.str());
+  //wrkbk->Dump(file_name.str());
 
   std::ofstream output;
   output.open(file_name.str());
@@ -473,31 +472,31 @@ void main_app::write_output(double mu, const std::vector<std::pair<size_t, doubl
   for(size_t i = 0; i < result.size(); ++i)
   {
     output << result[i].first << " " << result[i].second << std::endl;
-	}
-	output.close();
+  }
+  output.close();
 }
 
 void main_app::write_output(double mu, const std::vector<graph_randomization::undirected_graph>& result) const
 {
-	for(size_t i = 0; i < result.size(); ++i)
-	{
-		std::stringstream file_name;
-		file_name << (output_directory_.empty() ? "" : output_directory_ + "/") <<"N" << gr_data_.vertex_count_ 
-			<< "_p" << gr_data_.probability_ << "_u" << mu << "_T";
-		fs::create_directory(file_name.str());
-		file_name << "/final_graph__N" << gr_data_.vertex_count_ << "_p" << gr_data_.probability_ << "_u" << mu << "_" << i * graph_step_ << ".txt";
+  for(size_t i = 0; i < result.size(); ++i)
+  {
+    std::stringstream file_name;
+    file_name << (output_directory_.empty() ? "" : output_directory_ + "/") <<"N" << gr_data_.vertex_count_ 
+      << "_p" << gr_data_.probability_ << "_u" << mu << "_T";
+    fs::create_directory(file_name.str());
+    file_name << "/final_graph__N" << gr_data_.vertex_count_ << "_p" << gr_data_.probability_ << "_u" << mu << "_" << i * graph_step_ << ".txt";
 
-		std::ofstream file;
-		file.open(file_name.str());
-		if(false == file.is_open())
-		{
-			std::cerr << "Failed to open/create output file." << std::endl;
-			return;
-		}
-		boost::archive::text_oarchive oa(file);
-		oa << gr_data_.vertex_count_ << gr_data_.probability_;
-		boost::serialization::save(oa, result[i], 0);
+    std::ofstream file;
+    file.open(file_name.str());
+    if(!file.is_open())
+    {
+      std::cerr << "Failed to open/create output file." << std::endl;
+      return;
+    }
+    boost::archive::text_oarchive oa(file);
+    oa << gr_data_.vertex_count_ << gr_data_.probability_;
+    boost::serialization::save(oa, result[i], 0);
 
-		file.close();
-	}
+    file.close();
+  }
 }
